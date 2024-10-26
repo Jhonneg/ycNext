aca106782e7e51a5edede1bac52fcefa35f59186
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-10-26 02:53:47 -0300

    added startup card componet

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,21 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 66,
+      author: { _id: 1, name: "Meme" },
+      _id: 1,
+      description: "This is a descripton.",
+      image:
+        "https://media.istockphoto.com/id/2149530993/pt/foto/digital-human-head-concept-for-ai-metaverse-and-facial-recognition-technology.jpg?s=2048x2048&w=is&k=20&c=7S_g7mqboE7953csPX5-_K1ulcLil-4F7GioQGdHxMo=",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -18,6 +34,21 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
