b96764cd6d91952db79e3e8ab7c10b7f2c78ff75
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-10-27 08:06:05 -0300

    author and startup initial type

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,42 @@
+import { defineField, defineType } from "sanity";
+
+export const startup = defineType({
+  name: "startup",
+  title: "Startup",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    defineField({
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
+    }),
+    defineField({
+      name: "email",
+      type: "string",
+    }),
+    defineField({
+      name: "image",
+      type: "url",
+    }),
+    defineField({
+      name: "bio",
+      type: "text",
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+});
