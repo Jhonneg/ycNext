4e5fc92c24b512ca4baf456c6ecf321f8e37bc58
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-10-27 11:58:39 -0300

    sanity queries completed, config too

--- sanity/schemaTypes/startup.ts
+++ sanity/schemaTypes/startup.ts
@@ -22,21 +22,27 @@ export const startup = defineType({
       to: { type: "author" },
     }),
     defineField({
-      name: "email",
+      name: "views",
+      type: "number",
+    }),
+    defineField({
+      name: "description",
+      type: "text",
+    }),
+    defineField({
+      name: "category",
       type: "string",
+      validation: (Rule) =>
+        Rule.min(1).max(20).required().error("Please enter a category"),
     }),
     defineField({
       name: "image",
       type: "url",
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: "bio",
-      type: "text",
+      name: "pitch",
+      type: "markdown",
     }),
   ],
-  preview: {
-    select: {
-      title: "name",
-    },
-  },
 });
