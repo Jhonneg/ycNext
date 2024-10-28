a2949eaafa2a4d40e5bc8fe947f7394b0bbe6c15
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-10-28 04:20:50 -0300

    finished views write

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,22 @@
 import { client } from "@/sanity/lib/client";
+import { unstable_after as after } from "next/server";
 import Ping from "./Ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export default async function View({ id }: { id: string }) {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
+
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
