703c0aced522676f4355a8d00f9f44c0a3eab5a1
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-11-02 19:00:07 -0300

    initial profile markup and query

---
+++ app/(root)/user/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { auth } from "@/auth";
+import { client } from "@/sanity/lib/client";
+import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const session = await auth();
+  const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
+
+  if (!user) return notFound();
+
+  return (
+    <>
+      <section className="profile_container">
+        <div className="profile_card">
+          <div className="profile_title">
+            <h3 className="text-24-black uppercase text-center">{user.name}</h3>
+          </div>
+        </div>
+        <Image
+          src={user.image}
+          alt={user.name}
+          width={220}
+          height={220}
+          className="profile_image"
+        />
+      </section>
+    </>
+  );
+}
