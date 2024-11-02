3ba2a938c59ea6da39536a708c00bcbb6272224f
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-11-02 20:05:54 -0300

    added start up card skeleton and final touches

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,9 +1,10 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -32,21 +33,21 @@ export default function StartupCard({ post }: { post: StartupTypeCard }) {
           <Link href={`/user/${author?._id}`}>
             <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
-          <Link href={`/startup${_id}`}>
+          <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
         <Link href={`/user/${author?._id}`}>
           <Image
-            src="https://placehold.co/48x48"
-            alt="placeholder"
+            src={author?.image!}
+            alt={author?.name!}
             width={48}
             height={48}
             className="rounded-full"
           />
         </Link>
       </div>
-      <Link href={`/startup${_id}`}>
+      <Link href={`/startup/${_id}`}>
         <p className="startup-card_desc">{description}</p>
         <img src={image} alt="placeholder" className="startup-card_img" />
       </Link>
@@ -59,5 +60,17 @@ export default function StartupCard({ post }: { post: StartupTypeCard }) {
         </Button>
       </div>
     </li>
+  );
+}
+
+export function StartupCardSkeleton() {
+  return (
+    <>
+      {[0, 1, 2, 3, 4].map((index: number) => (
+        <li key={cn("skeleton", index)}>
+          <Skeleton className="startup-card_skeleton" />
+        </li>
+      ))}
+    </>
   );
 }
